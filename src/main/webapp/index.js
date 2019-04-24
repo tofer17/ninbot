7cac7dd0799a81efe05b22445c6f00beda5dbe43
tofer17 <tofer17@gmail.com>
2019-04-24 10:34:31 -0500

    Added Intro screen; displays upon load. HST is stubbed.

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -3,12 +3,31 @@
  *
  */
 
+
+function displayIntro () {
+
+	app.intro.style.display = "initial";
+	app.game.classList.add( "dim" );
+
+	app.controls.gameAction.classList.add( "hidden" );
+	app.controls.teleport.disabled = true;
+	app.controls.attack.disabled = true;
+	app.controls.lastStand.disabled = true;
+
+	app.controls.play.disabled = false;
+}
+
 function handleEvent ( event ) {
-	console.log( event );
+	console.log( event.target.id, event );
 }
 
 function main ( event ) {
 	window.app = new Object();
+
+	app.intro = document.querySelector( "#ninbot #intro" );
+	app.intro.hst = app.intro.querySelector( "hst" );
+
+	app.game = document.querySelector( "#ninbot #game" );
 
 	app.hud = document.querySelector( "#ninbot #hud");
 	app.hud.lives = app.hud.querySelector( "#lives" );
@@ -21,19 +40,22 @@ function main ( event ) {
 	app.grid = document.querySelector( "#ninbot #grid");
 
 	app.controls = document.querySelector( "#ninbot #controls");
-	app.controls.game = app.controls.querySelector( "#game" );
+	app.controls.gameAction = app.controls.querySelector( "#game_action" );
 	app.controls.teleport = app.controls.querySelector( "#teleport" );
 	app.controls.attack = app.controls.querySelector( "#attack" );
 	app.controls.lastStand = app.controls.querySelector( "#lastStand" );
-
-
+	// Keep "play" with other controls
+	app.controls.play = app.intro.querySelector( "#play" );
 
 
 	app.grid.addEventListener( "click", handleEvent );
-	app.controls.game.addEventListener( "click", handleEvent );
+	app.controls.gameAction.addEventListener( "click", handleEvent );
 	app.controls.teleport.addEventListener( "click", handleEvent );
 	app.controls.attack.addEventListener( "click", handleEvent );
 	app.controls.lastStand.addEventListener( "click", handleEvent );
+	app.controls.play.addEventListener( "click", handleEvent );
+
+	displayIntro();
 }
 
 window.addEventListener( "load", main );
